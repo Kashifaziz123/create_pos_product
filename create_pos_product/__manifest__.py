7c52fc7bbeaa0b53816a878c11eb7a466c170a7e
Kashifaziz123 <35768728+Kashifaziz123@users.noreply.github.com>
2023-03-11 11:57:29 +0500

    Update __manifest__.py

--- create_pos_product/__manifest__.py
+++ create_pos_product/__manifest__.py
@@ -1,28 +1,23 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Odoo POS Add Product",
-
     'summary': """
         Add product in odoo point of sale screen""",
-
     'description': """
         Long description of module's purpose
     """,
-
     'author': "Alhaditech",
     'website': "http://www.alhaditech.com",
-
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Point of Sale',
     'version': '0.1',
-
+    'price': 18, 'currency': 'USD',
     # any module necessary for this one to work correctly
     'depends': ['base', 'point_of_sale'],
     'images': ['static/description/cover_image.jpg'],
     # 'qweb': ['static/src/xml/btn_product.xml'],
-
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
